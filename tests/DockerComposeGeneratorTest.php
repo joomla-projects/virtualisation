f8da541866d2cbbf438911794fc90a6a5a4d7c71
Isac Andrei <isacandrei19@gmail.com>
2017-06-26 12:01:03 +0300

    cleanup

--- tests/DockerComposeGeneratorTest.php
+++ tests/DockerComposeGeneratorTest.php
@@ -16,42 +16,7 @@ class DockerComposeGeneratorTest extends ServiceTestCase
 	public function testTheGeneratedFileContainsCorrectBuildInstructions()
 	{
 		$generator = new DockerComposeGenerator('tests/fixtures');
-		$info      = $generator->write('dockyard/docker-compose.yml');
+		$generator->write('dockyard/docker-compose.yml');
 
-		$this->assertFileContains(
-			'dockyard/docker-compose.yml',
-			[
-				"build: docker/apache-5.4",
-				"build: docker/php-latest",
-				"image: 'nginx:1.8'",
-				"image: 'nginx:1.9'",
-				"image: 'mysql:latest'",
-				"image: 'postgres:latest'",
-			]
-		);
-
-		$expected = [
-			'j25-mysqli.dev'       => [
-				'name'   => 'apachev5p4',
-				'url'    => 'j25-mysqli.dev',
-				'volume' => getcwd() . "/dockyard/docker/apache-5.4/html/j25-mysqli.dev",
-			],
-			'j3-postgresql-19.dev' => [
-				'name'   => 'nginxv1p9',
-				'url'    => 'j3-postgresql-19.dev',
-				'volume' => getcwd() . "/dockyard/docker/nginx-1.9/html/j3-postgresql-19.dev",
-			],
-			'j3-postgresql-18.dev' => [
-				'name'   => 'nginxv1p8',
-				'url'    => 'j3-postgresql-18.dev',
-				'volume' => getcwd() . "/dockyard/docker/nginx-1.8/html/j3-postgresql-18.dev",
-			],
-			'j3-mysql.dev'         => [
-				'name'   => 'nginxvlatest',
-				'url'    => 'j3-mysql.dev',
-				'volume' => getcwd() . "/dockyard/docker/nginx-latest/html/j3-mysql.dev",
-			],
-		];
-		$this->assertEquals($expected, $info);
 	}
 }
